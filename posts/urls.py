979bb1cbedd7e12c1d9c8f6203cb394659601572
patrickryan331 <patrickryan331@gmail.com>
2024-08-10 14:04:10 -0400

    class 3 views

--- posts/urls.py
+++ posts/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path("list", views.PostListView.as_view(), name="list"),
     path("", views.HomePageView.as_view(), name="home"),
     path("about/", views.AboutPageView.as_view(), name="about"),
+    path("<int:pk>/", views.PostDetailView.as_view(), name="detail"),
+    path("new/", views.PostCreateView.as_view(), name="new"),
 ]
+
--- posts/views.py
+++ posts/views.py
@@ -5,21 +5,22 @@ from django.views.generic import (
     TemplateView,
 )
 from .models import Post
+# from django.urls import reverse
 
 
-class PostListView(ListView):
+class PostListView(ListView):                           #scan
     template_name = "posts/list.html"
     model = Post
 
-class PostDetailView(DetailView):
-    template_name = "posts/details.html"
+class PostDetailView(DetailView):                       #read single
+    template_name = "posts/detail.html"
     model = Post
 
-class PostCreateView(CreateView):
+class PostCreateView(CreateView):                       # create new records
     template_name = "posts/new.html"
     model = Post
     fields = ["title", "subtitle", "body"]
-    
+    # success_url = reverse("list")
 
 class HomePageView(TemplateView):
     template_name = "pages/home.html"
